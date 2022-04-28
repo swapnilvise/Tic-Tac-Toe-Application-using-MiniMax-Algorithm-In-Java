8ad21e86856b9b1dd2b333ca3b712695a4ff3229
Swapnil Vise <vise.s@northeastern.edu>
2022-04-27 23:44:19 -0400

    Added maven project of Menace

---
+++ src/test/java/MenaceBatchRun.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+import com.sun.tools.javac.Main;
+import org.junit.Test;
+
+public class MenaceBatchRun extends JFrame {
+
+    @Test
+    public void testPlay0() {
+
+    }
+
+}
