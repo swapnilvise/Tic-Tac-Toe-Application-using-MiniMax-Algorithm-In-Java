f0ef6bbf226c11879becea46fd9a387fc5e8f815
Swapnil Vise <vise.s@northeastern.edu>
2022-04-28 21:26:36 -0400

    Added testcases and updated the report after consulting the professor for extension

--- src/test/java/MenaceBatchRun.java
+++ src/test/java/MenaceBatchRun.java
@@ -2,12 +2,44 @@ import javax.swing.*;
 
 import com.sun.tools.javac.Main;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class MenaceBatchRun extends JFrame {
 
+    // Test Case to check if Possible Play class is returning correct row and column
     @Test
-    public void testPlay0() {
+    public void testPlay1() {
 
+        PossiblePlay pp = new PossiblePlay(1,2,2);
+        assertEquals(2,pp.getRow());
+        assertEquals(2,pp.getCol());
+        assertEquals(1,pp.getScore());
+    }
+
+    // Test Case to check that method Is Square Empty returns proper boolean value so that there is no overlap of a move
+    // in the actual game
+    @Test
+    public void testPlay2() {
+        TicTacToe TTT = new TicTacToe(3,3,3);
+        boolean check = TTT.squareIsEmpty(1,1);
+        assertEquals(true,check);
+    }
+
+    // Test case to check if wins method is returning false when the there is no win situation for symbol X
+    @Test
+    public void testPlay3() {
+        TicTacToe TTT = new TicTacToe(3,3,3);
+        boolean check = TTT.wins('X');
+        assertEquals(false,check);
+    }
+
+    // Test Case to check the method of evaluating the board
+    @Test
+    public void testPlay4() {
+        TicTacToe TTT = new TicTacToe(3,3,3);
+        int boradValue = TTT.evalBoard();
+        assertEquals(2,boradValue);
     }
 
 }
